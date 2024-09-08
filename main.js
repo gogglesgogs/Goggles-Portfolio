773503ff4e969e8c2965f24caa95a382cff1aac0
gogglesgogs <ngkhoa0075@gmail.com>
2024-09-08 20:34:56 +0700

    Commit #2

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ import './style.css';
 
 // `
 
-const theme = window.matchMedia('(prefers-color-scheme: light)').matches;
-
-if (theme) {
-	document.documentElement.setAttribute('data-theme', 'light');
-} else {
-	document.documentElement.setAttribute('data-theme', 'dark');
-}
+document.addEventListener("DOMContentLoaded", (event) => {
+	const theme = window.matchMedia('(prefers-color-scheme: light)').matches;
+	if (theme) {
+		document.documentElement.setAttribute('data-theme', 'light');
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+	}
+});
 
 const togglebuttonlight = document.querySelector('#light');
 const togglebuttondark = document.querySelector('#dark');
@@ -32,3 +33,39 @@ togglebutton.addEventListener('click', () => {
 window.addEventListener('scroll', () => {
 	document.querySelector('.scroll').style.display = 'none';
 });
+
+const gogglestxt = document.querySelector('#goggles');
+const gogglesimg = document.querySelector('#img-goggles');
+gogglestxt.addEventListener('mouseover', () => {
+	gogglesimg.style.animationName = 'slideIn';
+});
+gogglestxt.addEventListener('mouseleave', () => {
+	gogglesimg.removeAttribute('style');
+});
+
+function getTime() {
+	let now = new Date();
+	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	let dayOfWeek = days[now.getDay()];
+	let day = now.getDate();
+	let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	let month = months[now.getMonth()];
+	let hours = now.getHours();
+	let minutes = now.getMinutes();
+	let formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+	let formattedday = dayOfWeek + ", " + day + " " + month;
+	return [formattedday, formattedTime];
+}
+
+const daytimeel = document.querySelector('#daytime');
+
+function setTimetoElement() {
+	let [day, time] = getTime();
+	daytimeel.innerText = `${day} ${time}`;
+}
+
+setTimetoElement();
+
+setInterval(() => {
+	setTimetoElement();
+}, 30000);
