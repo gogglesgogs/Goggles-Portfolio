0dca38356c03596c48ecb5b6046e6ade6a3d5229
gogglesgogs <ngkhoa0075@gmail.com>
2024-09-22 18:03:18 +0700

    Commit #5

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
 import './style.css';
 
-const togglebuttonlight = document.querySelector('#light');
-const togglebuttondark = document.querySelector('#dark');
-const togglebutton = document.querySelector('#togglebtn');
+// document.querySelector('#app').innerHTML = /*html*/`
+// `;
+
 const gogglestxt = document.querySelector('#goggles');
 const gogglesimg = document.querySelector('#img-goggles');
 const daytimeel = document.querySelector('#daytime');
+const mybutton = document.getElementById('goTop');
+let lastScrollTop = 0;
 
+window.addEventListener('resize', checkViewportWidth);
 
-// document.querySelector('#app').innerHTML = `
-
-// `
-
-document.addEventListener("DOMContentLoaded", (event) => {
-	setTheme();
-	checkViewportWidth();
-	setTimetoElement();
-
-});
-
-window.addEventListener("resize", checkViewportWidth);
-
-togglebutton.addEventListener('click', () => {
-	const currentTheme = document.documentElement.dataset.theme;
-	if (currentTheme === 'light') {
-		togglebuttondark.style.display = 'none';
-		togglebuttonlight.style.display = 'block';
-		document.documentElement.setAttribute('data-theme', 'dark');
-	} else if (currentTheme === 'dark') {
-		togglebuttonlight.style.display = 'none';
-		togglebuttondark.style.display = 'block';
-		document.documentElement.setAttribute('data-theme', 'light');
+window.addEventListener('scroll', () => {
+	let scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	if (scrollTop < lastScrollTop && scrollTop > 100) {
+		mybutton.style.opacity = 1;
+	} else {
+		mybutton.style.opacity = 0;
 	}
+	lastScrollTop = scrollTop;
 });
+
+mybutton.addEventListener('click', ScrollToTop);
 
 window.addEventListener('scroll', () => {
 	document.querySelector('.scroll').style.display = 'none';
@@ -46,43 +35,49 @@ gogglestxt.addEventListener('mouseleave', () => {
 	gogglesimg.removeAttribute('style');
 });
 
-setInterval(() => {
-	setTimetoElement();
-}, 10000);
+document.getElementById('cards').onmousemove = (e) => {
+	for (const card of document.getElementsByClassName('card')) {
+		const rect = card.getBoundingClientRect(),
+			x = e.clientX - rect.left,
+			y = e.clientY - rect.top;
 
-function getTime() {
+		card.style.setProperty('--mouse-x', `${x}px`);
+		card.style.setProperty('--mouse-y', `${y}px`);
+	}
+};
+
+function setTime() {
 	let now = new Date();
-	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 	let dayOfWeek = days[now.getDay()];
 	let day = now.getDate();
-	let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+	let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 	let month = months[now.getMonth()];
 	let hours = now.getHours();
 	let minutes = now.getMinutes();
 	let formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-	let formattedday = dayOfWeek + ", " + day + " " + month;
-	return [formattedday, formattedTime];
-}
-
-function setTimetoElement() {
-	let [day, time] = getTime();
-	daytimeel.innerText = `${day} ${time}`;
-}
-
-function setTheme() {
-	const theme = window.matchMedia('(prefers-color-scheme: light)').matches;
-	if (theme) {
-		document.documentElement.setAttribute('data-theme', 'light');
-	} else {
-		document.documentElement.setAttribute('data-theme', 'dark');
-	}
+	let formattedDay = dayOfWeek + ', ' + day + ' ' + month;
+	daytimeel.innerHTML = formattedDay + ' ' + formattedTime;
 }
 
 function checkViewportWidth() {
 	var viewportWidth = window.innerWidth;
 	if (viewportWidth < 1380) {
-		document.getElementById("warning-sign").style.display = "flex";
+		document.getElementById('warning-sign').style.display = 'flex';
 	} else {
-		document.getElementById("warning-sign").style.display = "none";
+		document.getElementById('warning-sign').style.display = 'none';
 	}
 }
+
+function ScrollToTop() {
+	window.scrollTo({
+		top: 0,
+		left: 0,
+		behavior: "smooth",
+	  });
+}
+
+setInterval(setTime, 10000);
+
+setTime();
+checkViewportWidth();
